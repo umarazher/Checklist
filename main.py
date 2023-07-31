067577d4c1b69f1069ea76a66b4e49e333e8519e
Saqib <muhdsaqib1111@gmail.com>
2023-07-31 09:58:54 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask , render_template
+
+app=Flask(__name__)
+
+@app.route('/login',methods =['GET'])
+def login():
+    return render_template('login.html')
+
+
+
+@app.route('/home',methods =['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/widgets', methods=['GET'])
+def widgets():
+    return render_template('widgets.html')
+
+@app.route('/validation', methods=['GET'])
+def validation():
+    return render_template('validation.html')
+
+@app.route('/calendar', methods=['GET'])
+def calendar():
+    return render_template('pages/apps/calendar.html')
+
+@app.route('/email', methods=['GET'])
+def email():
+    return render_template('pages/apps/email.html')
+
+@app.route('/todo', methods=['GET'])
+def todo():
+    return render_template('pages/apps/todo.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages/samples/error-404.html')
+
+@app.route('/profile', methods = ['GET'])
+def profile():
+    return render_template('profile.html')
+
+@app.route('/feeds', methods = ['GET'])
+def feeds():
+    return render_template('pages/samples/timeline.html')
+
+@app.route('/', methods = ['GET'])
+def search():
+    return render_template('pages/samples/blank-page.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
